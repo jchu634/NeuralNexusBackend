46c31a18d25d988b0bcb0047c16586804ab2f195
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-03-05 00:26:58 +1300

    Switched to passlib

--- library/database/crud.py
+++ library/database/crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from requests import Response
 from time import time
-from argon2 import PasswordHasher
+from passlib.hash import argon2
 
 from library.database import models, schemas
 
@@ -13,8 +13,7 @@ def get_users_by_email(db: Session, email:str):
     return db.query(models.User).filter(models.User.email == email).first()
 
 def create_user(db: Session, username:str, email:str, password:str):    
-    db_user = models.User(username=username, email=email, password_hash=PasswordHasher().hash(password), is_admin=False)
-    
+    db_user = models.User(username=username, email=email, password_hash=argon2.hash(password), is_admin=False)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -29,7 +28,7 @@ def update_user_email(db:Session, username:str, email:str):
 
 def update_user_password(db:Session, username:str, password:str):
     db_user = get_users_by_username(username)
-    db_user.password = PasswordHasher().hash(password)
+    db_user.password = argon2.hash(password)
     db.commit()
     db.refresh(db_user)
     return db_user
