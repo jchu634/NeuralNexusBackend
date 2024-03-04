16bb72fee14c104f1f155cf63256aa828557f6e7
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-03-05 01:00:25 +1300

    Added function for admin check, added error handling for username clashes

--- library/auth/api.py
+++ library/auth/api.py
@@ -8,6 +8,7 @@ from library.config import Settings
 
 from datetime import datetime, timedelta, timezone
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 from passlib.context import CryptContext
 from passlib.hash import argon2
 from typing import Annotated
@@ -73,6 +74,25 @@ async def get_current_user(token: Annotated[str, Depends(Settings.OAUTH_SCHEME)]
         "email":user.email
     })
 
+async def check_if_user_admin(token: Annotated[str, Depends(Settings.OAUTH_SCHEME)], db: Annotated[Session, Depends(get_db)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, Settings.AUTH_SECRET_KEY, algorithms=[Settings.AUTH_ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = crud.get_users_by_username(db, token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user.is_admin
+
 async def get_current_active_user(
     current_user: Annotated[models.User, Depends(get_current_user)]
 ):
@@ -84,8 +104,16 @@ async def get_current_active_user(
 
 @auth_api.post("/create_user")
 def create_user(username:str, email:str, password:str, db: Session = Depends(get_db)):
-    crud.create_user(db, username,email, password)
-    return JSONResponse(content={"success":"user made"})
+    try:
+        crud.create_user(db, username,email, password)
+        logging.info(f"Created user {username}")
+        return JSONResponse(content={"success":"user made"})
+    except IntegrityError as e:
+        return ORJSONResponse(content={"error": "Username already taken"}, status_code=500)
+    except Exception as e:
+        logging.error(f"User creation error: {traceback.format_exc()}")
+        return ORJSONResponse(content={"error": "Internal Server Error"}, status_code=500)
+
 
 @auth_api.post("/token")
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)) -> Token:
@@ -110,7 +138,6 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: 
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-
 
 @auth_api.get("/users/me")
 async def read_users_me(current_user: Annotated[models.User, Depends(get_current_user)]):
