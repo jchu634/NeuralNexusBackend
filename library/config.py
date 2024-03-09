db4803316405e7d4aab1b557fef8bf68223964d4
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-03-09 23:44:19 +1300

    Added new env var IMAGE_DEFAULT_EXPIRY_PERIOD

--- library/config.py
+++ library/config.py
@@ -4,14 +4,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 import os
 
 library_path = os.path.dirname(os.path.abspath(__file__))
+
+
 class settingsModel(BaseSettings):
     ENV_TYPE: str = "temp"
-    UPLOAD_FOLDER: str =  os.path.join(library_path ,'static/uploads/')
-    STORAGE_FOLDER: str = os.path.join(library_path ,'static/storage/')
-    MODEL_FOLDER: str = os.path.join(library_path ,'models/')
+    UPLOAD_FOLDER: str = os.path.join(library_path, 'static/uploads/')
+    STORAGE_FOLDER: str = os.path.join(library_path, 'static/storage/')
+    MODEL_FOLDER: str = os.path.join(library_path, 'models/')
     ALLOWED_IMAGE_EXTENSIONS: Any = ['PNG', 'JPG', 'JPEG', 'GIF', 'WEBP']
+    IMAGE_DEFAULT_EXPIRY_PERIOD: int = 2592000
     CACHE_TIMEOUT_PERIOD: int = 900
-    
+
     OAUTH_SCHEME: Any = "temp"
     AUTH_SECRET_KEY: str = "blank"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
@@ -24,5 +27,6 @@ class settingsModel(BaseSettings):
             ".env",
         )
     )
+
 
 Settings = settingsModel()
