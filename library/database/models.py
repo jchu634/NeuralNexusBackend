3c3b478b389863c75e0d986ed8ee02fe2dcd55cf
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-03-09 23:22:59 +1300

    Updated database schema to include user settings and Many to one Image tables

--- library/database/models.py
+++ library/database/models.py
@@ -1,13 +1,40 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Table
 from sqlalchemy.types import JSON
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Mapped, relationship, DeclarativeBase, mapped_column
 
-from .database import Base
+from library.database.database import Base
+
+
+class Base(DeclarativeBase):
+    pass
+
 
 class User(Base):
-    __tablename__ = "users"
-    username = Column(String, primary_key=True, index=True)
+    __tablename__ = "user_table"
+    id: Mapped[str] = mapped_column(primary_key=True)
+    username = Column(String, index=True)
     email = Column(String, nullable=True)
     password_hash = Column(String)
     is_admin = Column(Boolean)
-    # disabled = Column(Boolean)
+    images: Mapped[list["Image"]] = relationship(back_populates="user")
+    settings: Mapped["Setting"] = relationship(back_populates="user")
+
+
+class Setting(Base):
+    __tablename__ = "settings_table"
+    id: Mapped[str] = mapped_column(
+        ForeignKey("user_table.id"), primary_key=True)
+    user: Mapped["User"] = relationship(back_populates="settings")
+    image_expiry = Column(Integer)
+
+
+class Image(Base):
+    __tablename__ = "image_table"
+    id: Mapped[str] = mapped_column(primary_key=True)
+    image_hash = Column(String, index=True)
+    image_name = Column(String, index=True)
+    expiry_time = Column(Integer, index=True)
+    user_id: Mapped[str] = mapped_column(ForeignKey("user_table.id"))
+    user: Mapped["User"] = relationship(
+        back_populates="images"
+    )
