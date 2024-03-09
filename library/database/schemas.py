b79a16631a755d1f547df4c1e75b47bbab90ee6d
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-03-09 23:24:34 +1300

    Formally added new definitions to schema.py

--- library/database/schemas.py
+++ library/database/schemas.py
@@ -1,7 +1,26 @@
 from pydantic import BaseModel
 
+
 class User(BaseModel):
+    id: str
     username: str
     email: str
     password_hash: str
     is_admin: bool
+    images: list["Image"]
+    settings: list["Setting"]
+
+
+class Setting(BaseModel):
+    id: str
+    user: "User"
+    image_expiry: int
+
+
+class Image(BaseModel):
+    id: str
+    image_hash: str
+    image_name: str
+    expiry_time = int
+    user_id: str
+    user: "User"
